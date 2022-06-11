fd35f100f2d0b4c9f96ba48f72ddaac9577f72f5
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-06-11 15:40:12 -0700

    routes, render templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask 
+from flask import Flask
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -7,5 +8,13 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+
+  @app.route('/hello')
+  def hello():
+    return 'hello world'
+    
+  # register routes
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
