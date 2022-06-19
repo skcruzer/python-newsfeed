1cdfecc0bf6faa2b5146a52c36e250c2505bc374
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-06-19 15:36:25 -0700

    login redirect

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app import db
 from app.models import User
 from app.db import get_db
@@ -33,5 +33,10 @@ def signup():
     # insert failed, so rollback and send error to front end
     db.rollback()
     return jsonify(message = 'Signup failed'), 500
+
+  # added session object to signup route
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
 
   return jsonify(id = newUser.id)
