95b636050387b3b424925e79e7833f8184f495d0
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-06-19 15:06:06 -0700

    errors handling, pending db connections

--- app/routes/api.py
+++ app/routes/api.py
@@ -4,6 +4,8 @@ from app import db
 from app.models import User
 from app.db import get_db
 
+import sys
+
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 # POST route to receive new user data
@@ -12,18 +14,24 @@ def signup():
   data = request.get_json()
   db = get_db()
   
-  # create new user
-  newUser = User(
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+  try:
+    # attempt to create new user
+    newUser = User(
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  # save in database
-  # method to prep the INSERT statement
-  db.add(newUser)
+    # save in database
+    # method to prep the INSERT statement
+    db.add(newUser)
+    #method to officially update database
+    db.commit()
 
-  #method to officially update database
-  db.commit()
+  except:
+    print(sys.exc_info()[0])
+    # insert failed, so rollback and send error to front end
+    db.rollback()
+    return jsonify(message = 'Signup failed'), 500
 
   return jsonify(id = newUser.id)
